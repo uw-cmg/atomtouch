fbc95754d8bd433013f373ae73cd73593401bd08
Justin Moeller <jgmoeller@wisc.edu>
2014-08-06 09:43:07 -0500

    Defined the graph in terms on screen coordinates

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -9,9 +9,9 @@ public class Graph : MonoBehaviour {
 	private float startTime;
 
 	//graph variables
-	private float xCoord = 3.75f;
-	private float yCoord = -2.3f;
-	private float width = 181.0f;
+	private float xCoord;
+	private float yCoord;
+	private float width = 180.0f;
 	private float height = 184.0f;
 	private float lineWidth = .015f;
 	private float zDepth = 5.0f;
@@ -26,6 +26,8 @@ public class Graph : MonoBehaviour {
 
 	void Start () {
 	
+		xCoord = Screen.width - 250;
+		yCoord = 70;
 		first = true;
 		maxDataPoints = (width / spacing) + 1;
 		dataPoints = new Queue ();
@@ -56,10 +58,10 @@ public class Graph : MonoBehaviour {
 	void OnGUI(){
 
 		GUI.Label (new Rect (Screen.width - 235, Screen.height - 280, 200, 20), "Temperature vs Time");
-		GUI.Label (new Rect (Screen.width - 305, Screen.height - 260, 100, 20), (StaticVariables.tempRangeHigh).ToString () + "K");
-		GUI.Label (new Rect (Screen.width - 305, Screen.height - 90, 100, 20), (StaticVariables.tempRangeLow).ToString () + "K");
-		GUI.Label (new Rect (Screen.width - 265, Screen.height - 70, 100, 20), (lowTime).ToString () + "s");
-		GUI.Label (new Rect (Screen.width - 90, Screen.height - 70, 100, 20), (highTime).ToString() + "s");
+		GUI.Label (new Rect (xCoord - 40, Screen.height - 260, 100, 20), (StaticVariables.tempRangeHigh).ToString () + "K");
+		GUI.Label (new Rect (xCoord - 40, Screen.height - 85, 100, 20), (StaticVariables.tempRangeLow).ToString () + "K");
+		GUI.Label (new Rect (xCoord - 5, Screen.height - 70, 100, 20), (lowTime).ToString () + "s");
+		GUI.Label (new Rect (xCoord + width - 5, Screen.height - 70, 100, 20), (highTime).ToString() + "s");
 		//print ("lowTimePosition: " + new Rect (Screen.width - 265, Screen.height - 70, 100, 20));
 		//print ("screen height: " + Screen.height);
 	}
@@ -68,12 +70,10 @@ public class Graph : MonoBehaviour {
 
 		Quaternion cameraRotation = Camera.main.transform.rotation;
 
-		//Vector3 upperLeft = cameraRotation * new Vector3 (xCoord, yCoord+height, zDepth);
-		//upperLeft += Camera.main.transform.position;
-		Vector3 upperLeft = camera.ScreenToWorldPoint (new Vector3 (Screen.width - 250, (254), 5.0f));
-		Vector3 lowerLeft = camera.ScreenToWorldPoint (new Vector3(Screen.width - 250, 70, 5.0f));
-		Vector3 upperRight = camera.ScreenToWorldPoint (new Vector3 (Screen.width - 69, (254), 5.0f));
-		Vector3 lowerRight = camera.ScreenToWorldPoint (new Vector3(Screen.width - 69, 70, 5.0f));
+		Vector3 upperLeft = camera.ScreenToWorldPoint (new Vector3 (xCoord, (yCoord+height), zDepth));
+		Vector3 lowerLeft = camera.ScreenToWorldPoint (new Vector3(xCoord, yCoord, zDepth));
+		Vector3 upperRight = camera.ScreenToWorldPoint (new Vector3 (xCoord + width, (yCoord+height), zDepth));
+		Vector3 lowerRight = camera.ScreenToWorldPoint (new Vector3(xCoord + width, yCoord, zDepth));
 		Color customColor = new Color (1.0f, 1.0f, 1.0f, .2f);
 		StaticVariables.DrawQuad (upperLeft, upperRight, lowerLeft, lowerRight, customColor, mat);
 
@@ -88,11 +88,11 @@ public class Graph : MonoBehaviour {
 			float firstPercentage = (float)dataPointArray[i] / (dataMaximum - dataMinimum);
 			float secondPercentage = (float)dataPointArray[i+1] / (dataMaximum - dataMinimum);
 
-			float firstYAddition = firstPercentage * 184.0f;
-			float secondYAddition = secondPercentage * 184.0f;
+			float firstYAddition = firstPercentage * height;
+			float secondYAddition = secondPercentage * height;
 
-			Vector3 firstPoint = camera.ScreenToWorldPoint(new Vector3(Screen.width - 250 + (i*spacing), 70 + firstYAddition, 5.0f));
-			Vector3 secondPoint = camera.ScreenToWorldPoint(new Vector3(Screen.width - 250 + ((i+1)*spacing), 70 + secondYAddition, 5.0f));
+			Vector3 firstPoint = camera.ScreenToWorldPoint(new Vector3(xCoord + (i*spacing), yCoord + firstYAddition, zDepth));
+			Vector3 secondPoint = camera.ScreenToWorldPoint(new Vector3(xCoord + ((i+1)*spacing), yCoord + secondYAddition, zDepth));
 			StaticVariables.DrawLine(firstPoint, secondPoint, Color.yellow, Color.yellow, lineWidth, mat);
 		}
 
