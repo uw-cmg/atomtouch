ddaa531019ca7d0df7f9894a2bb64382d3d005bf
Justin Moeller <jgmoeller@wisc.edu>
2014-08-21 16:57:15 -0500

    Added more comments on temperature and on drawing the lines between interactions

--- Assets/Scripts/VisualizeInteraction.cs
+++ Assets/Scripts/VisualizeInteraction.cs
@@ -1,4 +1,17 @@
-﻿using UnityEngine;
+﻿/**
+ * Class: VisualizeInteraction.cs
+ * Created by: Justin Moeller
+ * Description: This class draws the lines between the atoms. Because it only needs to draw lines between 
+ * every pair of atoms it only iterates through each distinct pair rather than every possible pair. (This 
+ * reduces the time spent from an O(N^2) to (1/2)O(N^2)). The lines are only drawn if the variable in StaticVariables
+ * drawBondLines is true. This variable is controlled from the user interface.
+ * 
+ * 
+ * 
+ **/
+
+
+using UnityEngine;
 using System.Collections;
 
 public class VisualizeInteraction : MonoBehaviour {
