7d957f17272b6caff618f9b3cff5a217ccd13837
Justin Moeller <jgmoeller@wisc.edu>
2014-08-22 14:59:43 -0500

    Added more comments to CreateEnvironment.cs

--- Assets/Scripts/CreateEnvironment.cs
+++ Assets/Scripts/CreateEnvironment.cs
@@ -46,14 +46,13 @@ public class CreateEnvironment : MonoBehaviour {
 	
 	void Start () {
 	
+		//precompute all of the sigma values in the system so it doesnt have to be done dynamically
 		centerPos = new Vector3 (0.0f, 0.0f, 0.0f);
 		StaticVariables.sigmaValues = new Dictionary<String, float> ();
-
 		for (int i = 0; i < molecules.Count; i++) {
 			Atom atomScript = molecules[i].GetComponent<Atom>();
 			StaticVariables.sigmaValues.Add(atomScript.atomName+atomScript.atomName, atomScript.sigma);
 		}
-
 		for (int i = 0; i < molecules.Count; i++) {
 			Atom firstAtomScript = molecules[i].GetComponent<Atom>();
 			for(int j = i+1; j < molecules.Count; j++){
@@ -73,11 +72,12 @@ public class CreateEnvironment : MonoBehaviour {
 			Instantiate(molecules[moleculeToSpawn].rigidbody, position, rotation);
 		}
 
+		//figure out the dimensions of the box based on the volume
 		width = (float)Math.Pow (volume, (1.0f / 3.0f));
 		height = (float)Math.Pow (volume, (1.0f / 3.0f));
 		depth = (float)Math.Pow (volume, (1.0f / 3.0f));
 		
-		//create the box
+		//create the bottom plane
 		Quaternion bottonPlaneRotation = Quaternion.Euler (0.0f, 0.0f, 0.0f);
 		Vector3 bottomPlanePos = new Vector3 (centerPos.x, centerPos.y - (height/2.0f), centerPos.z);
 		bottomPlane = Instantiate (plane, bottomPlanePos, bottonPlaneRotation) as GameObject;
@@ -85,34 +85,39 @@ public class CreateEnvironment : MonoBehaviour {
 		bottomPlane.name = "BottomPlane";
 		bottomPlane.tag = "Plane";
 
+		//create the top plane
 		Quaternion topPlaneRotation = Quaternion.Euler (0.0f, 180.0f, 180.0f);
 		Vector3 topPlanePos = new Vector3 (centerPos.x, centerPos.y + (height/2.0f), centerPos.z);
 		topPlane = Instantiate (plane, topPlanePos, topPlaneRotation) as GameObject;
 		topPlane.transform.localScale = new Vector3 (width / 10.0f, height / 10.0f, depth / 10.0f);
 		topPlane.name = "TopPlane";
 		topPlane.tag = "Plane";
-		
+
+		//create the back plane
 		Quaternion backPlaneRotation = Quaternion.Euler (270.0f, 0.0f, 0.0f);
 		Vector3 backPlanePos = new Vector3 (centerPos.x, centerPos.y, centerPos.z + (depth/2.0f));
 		backPlane = Instantiate (plane, backPlanePos, backPlaneRotation) as GameObject;
 		backPlane.transform.localScale = new Vector3 (width / 10.0f, depth / 10.0f, height / 10.0f);
 		backPlane.name = "BackPlane";
 		backPlane.tag = "Plane";
-		
+
+		//create the front plane
 		Quaternion frontPlaneRotation = Quaternion.Euler (90.0f, 0.0f, 0.0f);
 		Vector3 frontPlanePos = new Vector3 (centerPos.x, centerPos.y, centerPos.z - (depth/2.0f));
 		frontPlane = Instantiate (plane, frontPlanePos, frontPlaneRotation) as GameObject;
 		frontPlane.transform.localScale = new Vector3 (width / 10.0f, depth / 10.0f, height / 10.0f);
 		frontPlane.name = "FrontPlane";
 		frontPlane.tag = "Plane";
-		
+
+		//create the right plane
 		Quaternion rightPlaneRotation = Quaternion.Euler (0.0f, 0.0f, 90.0f);
 		Vector3 rightPlanePos = new Vector3 (centerPos.x + (width/2.0f), centerPos.y, centerPos.z);
 		rightPlane = Instantiate (plane, rightPlanePos, rightPlaneRotation) as GameObject;
 		rightPlane.transform.localScale = new Vector3 (height / 10.0f, width / 10.0f, depth / 10.0f);
 		rightPlane.name = "RightPlane";
 		rightPlane.tag = "Plane";
-		
+
+		//create the left plane
 		Quaternion leftPlaneRotation = Quaternion.Euler (0.0f, 0.0f, 270.0f);
 		Vector3 leftPlanePos = new Vector3 (centerPos.x - (width/2.0f), centerPos.y, centerPos.z);
 		leftPlane = Instantiate (plane, leftPlanePos, leftPlaneRotation) as GameObject;
@@ -120,16 +125,16 @@ public class CreateEnvironment : MonoBehaviour {
 		leftPlane.name = "LeftPlane";
 		leftPlane.tag = "Plane";
 		
-		//name the atoms
+		//name the atoms, simply make their name a number (i.e "0" or "1")
 		GameObject[] allMolecules = GameObject.FindGameObjectsWithTag("Molecule");
 		for (int i = 0; i < allMolecules.Length; i++) {
 			GameObject currAtom = allMolecules[i];
 			currAtom.name = i.ToString();
-			//currAtom.rigidbody.AddForce(new Vector3(0.0f, 5.0f, 0.0f), ForceMode.Impulse);
 		}
 
+		//create the lines that border the box and the text of width, height, and depth
+		//bottom line and text
 		Color lineColor = new Color (Color.yellow.r, Color.yellow.g, Color.yellow.b, .6f);
-		//create the lines and the labels
 		bottomText = Instantiate(textMeshPrefab, new Vector3(bottomPlanePos.x - 2.0f, bottomPlanePos.y - 1.0f, bottomPlanePos.z - (depth/2.0f)), Quaternion.identity) as TextMesh;
 		bottomText.text = width.ToString() + " Angstroms";
 		LineRenderer bottomLine = bottomText.transform.gameObject.AddComponent<LineRenderer> ();
@@ -138,6 +143,7 @@ public class CreateEnvironment : MonoBehaviour {
 		bottomLine.SetWidth(0.2F, 0.2F);
 		bottomLine.SetVertexCount(2);
 
+		//side line and text
 		sideText = Instantiate(textMeshPrefab, new Vector3(bottomPlanePos.x + (width/2.0f) + 1.0f, bottomPlanePos.y + (height*7/10.0f), bottomPlanePos.z - (depth/2.0f)), Quaternion.identity) as TextMesh;
 		sideText.text = VerticalText(height.ToString() + " Angstroms");
 		LineRenderer sideLine = sideText.transform.gameObject.AddComponent<LineRenderer> ();
@@ -146,7 +152,7 @@ public class CreateEnvironment : MonoBehaviour {
 		sideLine.SetWidth(0.2F, 0.2F);
 		sideLine.SetVertexCount(2);
 
-
+		//depth line and text
 		depthText = Instantiate(textMeshPrefab, new Vector3(centerPos.x + (width/2.0f), bottomPlanePos.y - 1.0f, centerPos.z - 2.0f), Quaternion.Euler(0.0f, -90.0f, 0.0f)) as TextMesh;
 		depthText.text = depth.ToString() + " Angstroms";
 		LineRenderer depthLine = depthText.transform.gameObject.AddComponent<LineRenderer> ();
@@ -155,30 +161,36 @@ public class CreateEnvironment : MonoBehaviour {
 		depthLine.SetWidth(0.2F, 0.2F);
 		depthLine.SetVertexCount(2);
 
+		//give all of the atoms a random velocity on startup
 		AtomTouchGUI atomTouchGUI = Camera.main.GetComponent<AtomTouchGUI> ();
 		atomTouchGUI.AtomKick ();
 	}
 
 	void Update () {
 
+		//when the volume of the box changes the width, height, and depth must change as well. The lines and the text must respond accordingly
 		width = (float)Math.Pow (volume, (1.0f / 3.0f));
 		height = (float)Math.Pow (volume, (1.0f / 3.0f));
 		depth = (float)Math.Pow (volume, (1.0f / 3.0f));
 		
 		CameraScript cameraScript = Camera.main.GetComponent<CameraScript> ();
 
+		//change the position of the bottom line
 		LineRenderer bottomLine = bottomText.GetComponent<LineRenderer> ();
 		bottomLine.SetPosition(0, new Vector3(bottomPlane.transform.position.x - (width/2.0f), bottomPlane.transform.position.y, bottomText.transform.position.z));
 		bottomLine.SetPosition(1, new Vector3(bottomPlane.transform.position.x + (width/2.0f), bottomPlane.transform.position.y, bottomText.transform.position.z));
-		
+
+		//change the position of the side line
 		LineRenderer sideLine = sideText.GetComponent<LineRenderer> ();
 		sideLine.SetPosition (0, new Vector3 (bottomPlane.transform.position.x + (width/2.0f), bottomPlane.transform.position.y, sideText.transform.position.z));
 		sideLine.SetPosition (1, new Vector3 (bottomPlane.transform.position.x + (width/2.0f), bottomPlane.transform.position.y + height, sideText.transform.position.z));
 
+		//change the position of the depth line
 		LineRenderer depthLine = depthText.GetComponent<LineRenderer> ();
 		depthLine.SetPosition(0, new Vector3(bottomPlane.transform.position.x + (width/2.0f), bottomPlane.transform.position.y, bottomPlane.transform.position.z - (depth/2.0f)));
 		depthLine.SetPosition(1, new Vector3(bottomPlane.transform.position.x + (width/2.0f), bottomPlane.transform.position.y, bottomPlane.transform.position.z + (depth/2.0f)));
 
+		//change the text of the labels and change their positions
 		bottomText.text = width.ToString() + " Angstroms";
 		sideText.text = VerticalText(height.ToString() + " Angstroms");
 		depthText.text = depth.ToString() + " Angstroms";
@@ -186,13 +198,15 @@ public class CreateEnvironment : MonoBehaviour {
 		depthText.transform.position = new Vector3 (bottomPlane.transform.position.x + (width / 2.0f), bottomPlane.transform.position.y - 1.0f, bottomPlane.transform.position.z - 2.0f);
 		bottomText.transform.position = new Vector3 (bottomPlane.transform.position.x - 2.0f, bottomPlane.transform.position.y - 1.0f, bottomPlane.transform.position.z - (depth / 2.0f));
 
+		//change the position of the box
 		rightPlane.transform.position = new Vector3 (initialCenterPos.x + (width/2.0f), initialCenterPos.y, initialCenterPos.z);
 		leftPlane.transform.position = new Vector3 (initialCenterPos.x - (width/2.0f), initialCenterPos.y, initialCenterPos.z);
 		bottomPlane.transform.position = new Vector3 (initialCenterPos.x, initialCenterPos.y - (height/2.0f), initialCenterPos.z);
 		topPlane.transform.position = new Vector3 (initialCenterPos.x, initialCenterPos.y + (height/2.0f), initialCenterPos.z);
 		backPlane.transform.position = new Vector3 (initialCenterPos.x, initialCenterPos.y, initialCenterPos.z + (depth/2.0f));
 		frontPlane.transform.position = new Vector3 (initialCenterPos.x, initialCenterPos.y, initialCenterPos.z - (depth/2.0f));
-		
+
+		//change the size of the box
 		bottomPlane.transform.localScale = new Vector3 (width / 10.0f, height / 10.0f, depth / 10.0f);
 		topPlane.transform.localScale = new Vector3 (width / 10.0f, height / 10.0f, depth / 10.0f);
 		backPlane.transform.localScale = new Vector3 (width / 10.0f, depth / 10.0f, height / 10.0f);
@@ -203,6 +217,7 @@ public class CreateEnvironment : MonoBehaviour {
 
 	}
 
+	//make the side text become vertical
 	String VerticalText(String text){
 		System.Text.StringBuilder verticalText = new System.Text.StringBuilder (text.Length * 2);
 		for (int i = 0; i < text.Length; i++) {
@@ -211,6 +226,7 @@ public class CreateEnvironment : MonoBehaviour {
 		return verticalText.ToString();
 	}
 
+	//resets the atoms to a random position and to the original number of atoms
 	public void ResetAtoms(){
 		GameObject[] allMolecules = GameObject.FindGameObjectsWithTag("Molecule");
 		for (int i = 0; i < allMolecules.Length; i++) {
@@ -228,8 +244,5 @@ public class CreateEnvironment : MonoBehaviour {
 		}
 		AtomTouchGUI atomTouchGUI = Camera.main.GetComponent<AtomTouchGUI> ();
 		atomTouchGUI.AtomKick();
-		//change the volume and the temperature back to their defaults
-//		volume = 8000.0f;
-//		TemperatureCalc.desiredTemperature = 200.0f;
 	}
 }
