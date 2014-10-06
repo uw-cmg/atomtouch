5b93ef271a20c78cc3f63728262c685e309e46df
amirhossein davoodi <amirhossein.davoody@gmail.com>
2014-10-06 13:12:08 -0500

    Bond drawing uses the static list of molecules

--- Assets/Scripts/VisualizeInteraction.cs
+++ Assets/Scripts/VisualizeInteraction.cs
@@ -21,23 +21,17 @@ public class VisualizeInteraction : MonoBehaviour {
 	void OnPostRender(){
 
 		if (StaticVariables.drawBondLines) {
-			GameObject[] allMolecules = GameObject.FindGameObjectsWithTag("Molecule");
-			
-			for (int i = 0; i < allMolecules.Length; i++) {
-				GameObject currAtom = allMolecules[i];
-				for(int j = i + 1; j < allMolecules.Length; j++){
-					GameObject atomNeighbor = allMolecules[j];
-					Atom atomScript = currAtom.GetComponent<Atom>();
-					if(Vector3.Distance(currAtom.transform.position, atomNeighbor.transform.position) < atomScript.BondDistance(atomNeighbor)){
+			for (int i = 0; i < Atom.AllMolecules.Count; i++) {
+				for(int j = i + 1; j < Atom.AllMolecules.Count; j++){
+					Atom currAtom = Atom.AllMolecules[i];
+					Atom neighborAtom = Atom.AllMolecules[j];
+					if((currAtom.transform.position - neighborAtom.transform.position).magnitude < currAtom.BondDistance(neighborAtom.gameObject)){
 						//draw a line from currAtom to atomNeighbor
-						Atom currAtomScript = currAtom.GetComponent<Atom>();
-						Atom neighAtomScript = atomNeighbor.GetComponent<Atom>();
-						StaticVariables.DrawLine (currAtom.transform.position, atomNeighbor.transform.position, Color.white, Color.white, .05f, mat);
+						StaticVariables.DrawLine (currAtom.transform.position, neighborAtom.transform.position, Color.white, Color.white, .05f, mat);
 					}
 				}
 			}
 		}
-
 	}
 
 
