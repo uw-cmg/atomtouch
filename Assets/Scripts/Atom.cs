e2174886b7515bfc718fe9efea6282881b898fde
Yucheng Tu <yuchengtu.uwm@gmail.com>
2015-01-09 12:09:29 -0600

    scene with updated hub prefab

--- Assets/Scripts/Atom.cs
+++ Assets/Scripts/Atom.cs
@@ -109,7 +109,8 @@ public abstract class Atom : MonoBehaviour
 		m_AllAtoms.Remove( atom );
 	}
 	
-	//this function takes care of double tapping, collision detection, and detecting OnMouseDown, OnMouseDrag, and OnMouseUp on iOS
+	//this function takes care of double tapping, collision detection, 
+	//and detecting OnMouseDown, OnMouseDrag, and OnMouseUp on iOS
 	void Update(){
 		
 		if (Application.platform == RuntimePlatform.IPhonePlayer) {
