4c05fd0615fccae85c6643dd32c6e2b4b2386c33
Pirolf <yuchengtu.uwm@gmail.com>
2015-04-01 15:58:40 -0500

    cleaned up two finger rotation

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -65,26 +65,14 @@ public class CameraScript : MonoBehaviour {
 			return;
 		}
 		//Debug.Log("resumed");
-		if(true){
-		//if (Application.isMobilePlatform) {
+		//if(true){
+		if (Application.isMobilePlatform) {
 			if(Input.touchCount == 1 ){
-				Debug.Log("ONE FINGER");
 				Touch touch = Input.GetTouch (0);
 				//if one finger lifed up during two finger rotation,
 				//ignore the remaining finger
 				//until it ends too
-				if(twoFingerRotating){
-					if(touch.phase != TouchPhase.Ended 
-						&& touch.phase != TouchPhase.Canceled ){
-						return;
-					}else{
-						twoFingerRotating = false;
-						return;
-					}
-				}
-				else if (touch.phase == TouchPhase.Moved) {
-					UpdateCamera();
-				}
+				ProcessOneFingerRotate(touch);
 			}else if(Input.touchCount == 2){
 				//Two finger rotation
 				//how to differentiate rotate from zoom?
@@ -93,62 +81,105 @@ public class CameraScript : MonoBehaviour {
 				Touch finger1 = Input.GetTouch(1);
 				if(finger0.phase == TouchPhase.Moved
 					|| finger1.phase == TouchPhase.Moved){
-				
-					Vector2 oldFingerLeft, oldFingerRight;
-					Vector2 newFingerLeft, newFingerRight;
-					//determine left and right
-					if(finger1.position.x-finger1.deltaPosition.x
-						> finger0.position.x-finger0.deltaPosition.x){
-
-						newFingerLeft = finger0.position;
-						newFingerRight = finger1.position;
-						oldFingerLeft = finger0.position - finger0.deltaPosition;
-						oldFingerRight = finger1.position - finger1.deltaPosition;
-					}else{
-						newFingerLeft = finger1.position;
-						newFingerRight = finger0.position;
-						oldFingerLeft = finger1.position - finger1.deltaPosition;
-						oldFingerRight = finger0.position - finger0.deltaPosition;
-					}
-
-					float diffX = oldFingerLeft.x - oldFingerRight.x;
-					if(Mathf.Abs(diffX) < Screen.width * 0.35f){
-						return;
-					}
-					twoFingerRotating = true;
-					Vector2 v_old = oldFingerRight-oldFingerLeft;
-					Vector2 v_new = newFingerRight-newFingerLeft;
 					
-
-					float angle = Vector2.Angle(v_old, v_new);
-					if(newFingerRight.y > oldFingerRight.y
-						&& newFingerLeft.y < oldFingerLeft.y){
-						//counterclockwise
-						angle *= -1;
-						Camera.main.gameObject.transform.eulerAngles
-						+= new Vector3(0,0,angle);
-					}else if(newFingerRight.y < oldFingerRight.y
-						&& newFingerLeft.y > oldFingerLeft.y){
-						//clockwise
-						Camera.main.gameObject.transform.eulerAngles
-						+= new Vector3(0,0,angle);
-					}
-					Debug.Log("Angle: " + angle);
-					
+					ProcessTwoFingerRotate(finger0, finger1);
 				}
 				
 			}
 		}
 		else{
-			if(Input.GetMouseButton(0)){
+			if(Input.GetMouseButton(0))
 				UpdateCamera();
-			}
-
 		}
-		//ChangeBackgroundColor ();
 
 	}
+	public void ProcessOneFingerRotate(Touch touch){
+		if(twoFingerRotating){
+			if(touch.phase != TouchPhase.Ended 
+				&& touch.phase != TouchPhase.Canceled ){
+				return;
+			}else{
+				twoFingerRotating = false;
+				return;
+			}
+		}
+		else if (touch.phase == TouchPhase.Moved) {
+			UpdateCamera();
+		}
+	}
+	public void ProcessTwoFingerRotate(Touch finger0, Touch finger1){
+		Vector2 oldFingerLeft, oldFingerRight;
+		Vector2 newFingerLeft, newFingerRight;
+		//determine left and right
+		DeterminLeftRightFingers(
+			finger0, finger1,
+			out oldFingerLeft, 
+			out oldFingerRight,
+			out newFingerLeft,
+			out newFingerRight
+		);
 
+		float diffX = oldFingerLeft.x - oldFingerRight.x;
+		//if the two fingers are too close, leave it for pinchzoom
+		if(Mathf.Abs(diffX) < Screen.width * 0.35f){
+			return;
+		}
+		TwoFingerRotate(
+			oldFingerLeft,oldFingerRight,
+			newFingerLeft,newFingerRight
+		);
+	}
+
+	public void TwoFingerRotate(
+		Vector2 oldFingerLeft,
+		Vector2 oldFingerRight,
+		Vector2 newFingerLeft,
+		Vector2 newFingerRight
+	){
+		twoFingerRotating = true;
+		Vector2 v_old = oldFingerRight-oldFingerLeft;
+		Vector2 v_new = newFingerRight-newFingerLeft;
+		
+
+		float angle = Vector2.Angle(v_old, v_new);
+		if(newFingerRight.y > oldFingerRight.y
+			&& newFingerLeft.y < oldFingerLeft.y){
+			//counterclockwise
+			angle *= -1;
+			Camera.main.gameObject.transform.eulerAngles
+			+= new Vector3(0,0,angle);
+		}else if(newFingerRight.y < oldFingerRight.y
+			&& newFingerLeft.y > oldFingerLeft.y){
+			//clockwise
+			Camera.main.gameObject.transform.eulerAngles
+			+= new Vector3(0,0,angle);
+		}
+	}
+	//given two finger touches, determine which is left and which is right
+
+	public void DeterminLeftRightFingers(
+		Touch finger0,
+		Touch finger1,
+		out Vector2 oldFingerLeft, 
+		out Vector2 oldFingerRight,
+		out Vector2 newFingerLeft,
+		out Vector2 newFingerRight){
+
+		//determine left and right
+		if(finger1.position.x-finger1.deltaPosition.x
+			> finger0.position.x-finger0.deltaPosition.x){
+
+			newFingerLeft = finger0.position;
+			newFingerRight = finger1.position;
+			oldFingerLeft = finger0.position - finger0.deltaPosition;
+			oldFingerRight = finger1.position - finger1.deltaPosition;
+		}else{
+			newFingerLeft = finger1.position;
+			newFingerRight = finger0.position;
+			oldFingerLeft = finger1.position - finger1.deltaPosition;
+			oldFingerRight = finger0.position - finger0.deltaPosition;
+		}
+	}
 	//this function is called when the user double taps an atom
 	public void setCameraCoordinates(Transform objTransform){
 		CreateEnvironment createEnvironment = Camera.main.GetComponent<CreateEnvironment> ();
@@ -162,7 +193,6 @@ public class CameraScript : MonoBehaviour {
 
 		Camera.main.transform.RotateAround(Vector3.zero, Camera.main.transform.rotation * Vector3.up, rotateSensitivityUpDown *x);
 		Camera.main.transform.RotateAround(Vector3.zero, Camera.main.transform.rotation * Vector3.left, rotateSensitivityLeftRight *y);
-	
 	
 	}
 
