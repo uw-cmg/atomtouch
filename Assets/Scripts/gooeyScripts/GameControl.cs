2361a001e37f82f8cddf4ba9c1a1b5b26999d5f6
Pirolf <yuchengtu.uwm@gmail.com>
2015-04-06 16:34:15 -0500

    timer ticking

--- Assets/Scripts/gooeyScripts/GameControl.cs
+++ Assets/Scripts/gooeyScripts/GameControl.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 public class GameControl : MonoBehaviour{
 	public static GameControl self;
+	public float allowedTime = 300f; //in seconds
+	public float timeRemaining;
+	public Text timer;
 	public enum GameState{
 		Running,
-		AddingAtom
+		AddingAtom,
+		Win,
+		Lose
 	};
 	public static int gameState;
 	Ray ray;
@@ -12,6 +18,7 @@ public class GameControl : MonoBehaviour{
 	public GameObject AtomToBeAdded{
 		get{return atomToBeAdded;}
 	}
+
 	void Awake(){
 		self = this;
 
@@ -19,58 +26,72 @@ public class GameControl : MonoBehaviour{
 	void Start(){
 		gameState = (int)GameState.Running;
 		atomToBeAdded = null;
+		timeRemaining = allowedTime;
+		timer.text = timeRemaining+"";
+	}
+	public void UpdateTimer(){
+		timeRemaining -= Time.deltaTime;
+		timer.text = timeRemaining + "";
 	}
 	void Update(){
-			if(gameState == (int)GameState.Running && atomToBeAdded != null){
-				//do nothing
-				Vector3 dir = atomToBeAdded.transform.position - Camera.main.gameObject.transform.position;
-				ray = new Ray(Camera.main.gameObject.transform.position, dir);
-				Debug.DrawRay(ray.origin, ray.direction * dir.magnitude, Color.yellow);
-			}else if(gameState == (int)GameState.AddingAtom){
+		
+		if(timeRemaining <= 0.01){
+			//end of game
+			//show win or lose
+			gameState = (int)GameState.Lose;
+			return;
+		}
+		UpdateTimer();
+		if(gameState == (int)GameState.Running && atomToBeAdded != null){
+			//do nothing
+			Vector3 dir = atomToBeAdded.transform.position - Camera.main.gameObject.transform.position;
+			ray = new Ray(Camera.main.gameObject.transform.position, dir);
+			Debug.DrawRay(ray.origin, ray.direction * dir.magnitude, Color.yellow);
+		}else if(gameState == (int)GameState.AddingAtom){
 
-				Vector3 mouseposWithDistance 
-					= new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.1f);
-				//mouseposWithDistance.z = 10f ;
-				Vector3 atomPos = Camera.main.ScreenToWorldPoint(mouseposWithDistance);
-				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
-				Vector3 dir = ray.direction;
-				//Debug.Log("ray dir: " + dir);
-				atomPos = ray.origin + ray.direction * 10f;
-				//check collision
-				float r = atomToBeAdded.GetComponent<SphereCollider>().radius ;
-				Vector3 sphereCastDir = atomPos-atomToBeAdded.transform.position;
-				dir.Normalize();
-				RaycastHit[] hits
-					= Physics.SphereCastAll(atomPos, r, sphereCastDir, sphereCastDir.magnitude);
-				bool hitsBox = false;
-				foreach(RaycastHit hit in hits){
-					if(hit.collider.gameObject.name == "Cube"){
-						hitsBox = true;
-						break;
-					}
+			Vector3 mouseposWithDistance 
+				= new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.1f);
+			//mouseposWithDistance.z = 10f ;
+			Vector3 atomPos = Camera.main.ScreenToWorldPoint(mouseposWithDistance);
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+			Vector3 dir = ray.direction;
+			//Debug.Log("ray dir: " + dir);
+			atomPos = ray.origin + ray.direction * 10f;
+			//check collision
+			float r = atomToBeAdded.GetComponent<SphereCollider>().radius ;
+			Vector3 sphereCastDir = atomPos-atomToBeAdded.transform.position;
+			dir.Normalize();
+			RaycastHit[] hits
+				= Physics.SphereCastAll(atomPos, r, sphereCastDir, sphereCastDir.magnitude);
+			bool hitsBox = false;
+			foreach(RaycastHit hit in hits){
+				if(hit.collider.gameObject.name == "Cube"){
+					hitsBox = true;
+					break;
 				}
-
-				if(!hitsBox ){
-					atomToBeAdded.transform.position = atomPos;
-				}
-				
-				atomToBeAdded.GetComponent<Rigidbody>().velocity = Vector3.zero;
-				Color solidColor = 
-					atomToBeAdded.GetComponent<MeshRenderer>().material.color;
-				atomToBeAdded.GetComponent<MeshRenderer>().material.color
-					= new Color(solidColor.r, solidColor.g, solidColor.b, 60/255);
-				Debug.Log(atomToBeAdded.GetComponent<MeshRenderer>().material.color);
-				//if on non mobile: mouse left click
-				if(Input.GetMouseButtonDown(0)){
-					FinishAddingAtom();
-					//yield return new WaitForSeconds(1f);
-					AtomPhysics.self.Ions.Add(atomToBeAdded);
-					SetGameStateRunning();
-				}
-				//if on mobile: mouse up
-
 			}
+
+			if(!hitsBox ){
+				atomToBeAdded.transform.position = atomPos;
+			}
+			
+			atomToBeAdded.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			Color solidColor = 
+				atomToBeAdded.GetComponent<MeshRenderer>().material.color;
+			atomToBeAdded.GetComponent<MeshRenderer>().material.color
+				= new Color(solidColor.r, solidColor.g, solidColor.b, 60/255);
+			Debug.Log(atomToBeAdded.GetComponent<MeshRenderer>().material.color);
+			//if on non mobile: mouse left click
+			if(Input.GetMouseButtonDown(0)){
+				FinishAddingAtom();
+				//yield return new WaitForSeconds(1f);
+				AtomPhysics.self.Ions.Add(atomToBeAdded);
+				SetGameStateRunning();
+			}
+			//if on mobile: mouse up
+
+		}
 		
 		
 		
